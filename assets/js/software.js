aa6683c0226cb62a9dc4f9c74316f3b08ac09537
Hadrien Gourlé <gourlehadrien@gmail.com>
2020-08-04 11:03:42 +0200

    add mag to soft list

--- assets/js/software.js
+++ assets/js/software.js
@@ -3,30 +3,38 @@ const software = new Vue({
   data: {
     show: false,
     soft: [
-        {
-            key: 0,
-            name: "InSilicoSeq",
-            description: "A sequencing simulator",
-            doi: "unpublished",
-            repo: "https://github.com/HadrienG/InSilicoSeq",
-            doc: "https://insilicoseq.readthedocs.io/en/latest/"
-        },
-        {
-            key: 1,
-            name: "taxadb",
-            description: "Locally query the ncbi taxonomy",
-            doi: "unpublished",
-            repo: "https://github.com/HadrienG/taxadb",
-            doc: "https://taxadb.readthedocs.io/en/latest/"
-        },
-        {
-            key: 2,
-            name: "nanoflow",
-            description: "De novo assembly of nanopore reads using nextflow",
-            doi: "unpublished",
-            repo: "https://github.com/HadrienG/nanoflow",
-            doc: "https://github.com/HadrienG/nanoflow"
-        }
+      {
+        key: 0,
+        name: "InSilicoSeq",
+        description: "A sequencing simulator",
+        doi: "unpublished",
+        repo: "https://github.com/HadrienG/InSilicoSeq",
+        doc: "https://insilicoseq.readthedocs.io/en/latest/"
+      },
+      {
+        key: 1,
+        name: "taxadb",
+        description: "Locally query the ncbi taxonomy",
+        doi: "unpublished",
+        repo: "https://github.com/HadrienG/taxadb",
+        doc: "https://taxadb.readthedocs.io/en/latest/"
+      },
+      {
+        key: 2,
+        name: "nanoflow",
+        description: "De novo assembly of nanopore reads using nextflow",
+        doi: "unpublished",
+        repo: "https://github.com/HadrienG/nanoflow",
+        doc: "https://github.com/HadrienG/nanoflow"
+      },
+      {
+        key: 3,
+        name: "mag",
+        description: "Assembly and binning of metagenomes",
+        doi: "10.5281/zenodo.3589528",
+        repo: "https://github.com/nf-core/mag",
+        doc: "https://nf-co.re/mag"
+      }
     ]
   }
 });
